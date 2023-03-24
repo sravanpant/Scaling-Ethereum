cd47fd789082b464ff050bb85850f9aaab4d30cd
Sravan Pant <sravan.pant5@gmail.com>
2023-03-24 21:29:57 +0530

    changed cryptofi

--- components/Component.jsx
+++ components/Component.jsx
@@ -1,43 +1,39 @@
 import React, { useEffect, useState } from "react";
 import PieChart from "./PieChart";
 import TableComponent from "./TableComponent";
+import { useRouter } from "next/router";
 
 const Component = (props) => {
-  const [domLoaded, setDomLoaded] = useState(false);
+  // const [domLoaded, setDomLoaded] = useState(false);
+  const router = useRouter();
 
-  useEffect(() => {
-    setDomLoaded(true);
-  }, []);
+  // useEffect(() => {
+  //   setDomLoaded(true);
+  // }, []);
 
-  if (typeof window === "object") {
-    console.log(window.location.pathname);
-
-    if (window.location.pathname == "/CryptoFi") {
-      return (
-        <>
-          {domLoaded && (
-            <div>
-              <PieChart
-                one={props.assetOne}
-                two={props.assetTwo}
-                three={props.assetThree}
-                four={props.assetFour}
-                valueOne={props.assetValueOne}
-                valueTwo={props.assetValueTwo}
-                valueThree={props.assetValueThree}
-                valueFour={props.assetValueFour}
-              />
-            </div>
-          )}
-        </>
-      );
-    } else if (window.location.pathname == "/LendFi") {
-      return (
+  if (router.pathname == "/CryptoFi") {
+    return (
+      <>
         <div>
-          <TableComponent />
+          <PieChart
+            one={props.assetOne}
+            two={props.assetTwo}
+            three={props.assetThree}
+            four={props.assetFour}
+            valueOne={props.assetValueOne}
+            valueTwo={props.assetValueTwo}
+            valueThree={props.assetValueThree}
+            valueFour={props.assetValueFour}
+          />
         </div>
-      );
-    }
+      </>
+    );
+  } else if (router.pathname == "/LendFi") {
+    return (
+      <div>
+        <TableComponent />
+      </div>
+    );
   }
 };
 
